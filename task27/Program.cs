36558cc909774eb6ab92488b3eac5c09b5e38b0b
valeria <leraalex20@mail.ru>
2022-08-21 15:56:39 +0300

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Ввести целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num3 = 0;
+for (int num2 = 1; num1 > 0; num2++)
+{
+    num3 = num3 = num1 % 10;
+    num1 = num1 / 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе будет {num3}");
